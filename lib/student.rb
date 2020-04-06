80be0d2c8f9806e22aaa4cf3061864c7ec2a84c5
Derric Fragosa <fderric@gmail.com>
2020-04-06 00:13:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,36 +3,28 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id 
   
-<<<<<<< HEAD
-=======
-  @@all = []
-  
->>>>>>> cf48935224d6d7c877c1fcd0eea4e5b9c62cdb18
   def initialize(id = nil, name, grade)
     @name = name
     @grade = grade
     @id = id 
-<<<<<<< HEAD
+
   end 
 
   def self.create_table
   sql = <<-SQL
         CREATE TABLE students (id INTEGER PRIMARY KEY,
         name TEXT, grade TEXT);
-=======
-    @@all << self 
+        SQL
   end 
 
   def self.create_table
   sql = <<~SQL
         CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY,
         name TEXT, grade INTEGER);
->>>>>>> cf48935224d6d7c877c1fcd0eea4e5b9c62cdb18
         SQL 
         DB[:conn].execute(sql)
   end
 
-<<<<<<< HEAD
   def self.drop_table
     sql = <<-SQL 
       DROP TABLE students;
@@ -50,7 +42,7 @@ class Student
   end
   
   def self.create(name:, grade:)
-=======
+
   def self.drop__table
     sql = <<~SQL 
       DROP TABLE students;
@@ -69,7 +61,6 @@ class Student
   end
   
   def self.create(name, grade)
->
     student = Student.new(name, grade)
     student.save
     student
