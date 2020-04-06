a16e4d1b64477a57bf458528d416b90630f383b6
Derric Fragosa <fderric@gmail.com>
2020-04-06 00:26:27 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,7 @@
 require 'bundler'
 Bundler.require
 
-<<<<<<< HEAD
-
-=======
 require 'sqlite3'
->>>>>>> cf48935224d6d7c877c1fcd0eea4e5b9c62cdb18
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
