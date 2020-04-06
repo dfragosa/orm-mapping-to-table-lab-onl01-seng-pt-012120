c30b630ad05ebaf34b5f50113e87040ed3b3f81c
Derric Fragosa <fderric@gmail.com>
2020-04-06 01:18:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,43 +1,45 @@
+require "pry"
+
 class Student
-  
-  attr_accessor :name, :grade
-  attr_reader :id 
-  
-  def initialize(id = nil, name, grade)
-    @id = id
-    @name = name
-    @grade = grade
-     
-  end 
+attr_accessor :name, :grade
+attr_reader :id
+  # Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]
 
-  def self.create_table
+def initialize(id = nil, name, grade)
+@name = name
+@grade = grade
+@id = id
+end
+
+def self.create_table
   sql = <<-SQL
-        CREATE TABLE students (id INTEGER PRIMARY KEY,
-        name TEXT, grade TEXT);
-        SQL
-        DB[:conn].execute(sql)
-  end 
+  create table students(id integer primary key, name text, grade text);
+  SQL
+  DB[:conn].execute(sql)
 
-  def self.drop_table
-  sql = <<-SQL 
-      DROP TABLE students;
-      SQL 
-      DB[:conn].execute(sql)
-  end
-  
-  def save
+end
+
+def self.drop_table
+sql = <<-SQL
+drop table students;
+SQL
+DB[:conn].execute(sql)
+end
+
+def save
   sql = <<-SQL
-      INSERT INTO students(name, grade)
-      VALUES (?,?);
-    SQL 
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
-  end
+  insert into students (name,grade) values(?,?);
+  SQL
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
+end
 
-  
-  def self.create(name, grade)
-    student = Student.new(name, grade)
-    student.save
-    student
-  end
+def self.create(name:, grade:)
+student = Student.new(name, grade)
+student.save
+student
+end
+
+
 end
