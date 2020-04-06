fa5119152292a161b09158040c35b04c401b348d
Derric Fragosa <fderric@gmail.com>
2020-04-06 00:25:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,15 +15,8 @@ class Student
         CREATE TABLE students (id INTEGER PRIMARY KEY,
         name TEXT, grade TEXT);
         SQL
-  end 
-
-  def self.create_table
-  sql = <<~SQL
-        CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY,
-        name TEXT, grade INTEGER);
-        SQL 
         DB[:conn].execute(sql)
-  end
+  end 
 
   def self.drop_table
     sql = <<-SQL 
@@ -40,25 +33,7 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
   end
-  
-  def self.create(name:, grade:)
 
-  def self.drop__table
-    sql = <<~SQL 
-      DROP TABLE students;
-      SQL 
-      DB[:conn].execute(sl)
-  end
-  
-  def save
-    sql = <<SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?);
-    SQL 
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
- 
-  end
   
   def self.create(name, grade)
     student = Student.new(name, grade)
